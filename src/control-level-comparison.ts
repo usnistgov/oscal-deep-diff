192e62c72bca053b2d2a2ebb7883f01beeef0ae5
Nikita Wootten <nikita.wootten@nist.gov>
2021-07-26 12:12:07 -0400

    Control-level-comp. now handles array subchanges

--- src/control-level-comparison.ts
+++ src/control-level-comparison.ts
@@ -1,17 +1,20 @@
-import { ArrayChanged, PropertyChanged, PropertyLeftOnly, PropertyRightOnly } from './comparisons';
+import { ArrayChanged, Change, PropertyChanged, PropertyLeftOnly, PropertyRightOnly } from './comparisons';
 import { TrackedElement } from './tracked';
+import { JSONValue } from './utils';
+
+interface ChangeDetails {
+    field: string;
+    leftValue?: JSONValue;
+    rightValue?: JSONValue;
+}
 
 interface ControlLevelComparison {
-    leftIdentifiers?: { [key: string]: unknown };
-    rightIdentifiers?: { [key: string]: unknown };
+    leftIdentifiers?: { [key: string]: string };
+    rightIdentifiers?: { [key: string]: string };
 
     status: 'ok' | 'added' | 'withdrawn' | 'changed';
 
-    changes?: {
-        field: string;
-        leftValue?: string;
-        rightValue?: string;
-    }[];
+    changes?: ChangeDetails[];
 
     moveDetails?: {
         leftParentIdentifiers: { [key: string]: unknown };
@@ -19,20 +22,62 @@ interface ControlLevelComparison {
     };
 }
 
-// const exampleControlLevelComparison: ControlLevelComparison = {
-//     leftIdentifiers: {
-//         id: 'AC-1',
-//         title: 'Policy and Procedures',
-//     },
-//     status: 'changed',
-//     changes: [
-//         {
-//             field: 'title',
-//             leftValue: 'Access Control Policy and Procedures',
-//             rightValue: 'Policy and Procedures',
-//         },
-//     ],
-// };
+function sortControlLevelComparison(comparison: ControlLevelComparison[], primaryIdentifier = 'id') {
+    comparison.sort((a, b) => {
+        // extract id from control comparisons. First right identifiers, then left identifiers
+        // (in the case of a withdrawn control)
+        const aId = a.rightIdentifiers?.[primaryIdentifier] ?? a.leftIdentifiers?.[primaryIdentifier] ?? '';
+        const bId = b.rightIdentifiers?.[primaryIdentifier] ?? b.leftIdentifiers?.[primaryIdentifier] ?? '';
+
+        return aId > bId ? 1 : aId < bId ? -1 : 0;
+    });
+}
+
+function FlattenControlChanges(
+    change: Change,
+    detailsList: ChangeDetails[],
+    leftParentPointer: string,
+    rightParentPointer: string,
+): void {
+    if (change instanceof ArrayChanged) {
+        change.leftOnly.forEach((leftOnly) =>
+            detailsList.push({
+                field: leftOnly.leftPointer.slice(leftParentPointer.length + 1),
+                leftValue: leftOnly.leftElement,
+            }),
+        );
+
+        change.rightOnly.forEach((rightOnly) =>
+            detailsList.push({
+                field: rightOnly.rightPointer.slice(rightParentPointer.length + 1),
+                rightValue: rightOnly.rightElement,
+            }),
+        );
+
+        change.subChanges.forEach((subChange) =>
+            subChange.changes.forEach((subChangeChanges) =>
+                FlattenControlChanges(subChangeChanges, detailsList, leftParentPointer, rightParentPointer),
+            ),
+        );
+    } else {
+        const changeOutput: ChangeDetails = {
+            field:
+                change instanceof PropertyLeftOnly
+                    ? change.leftPointer.slice(leftParentPointer.length + 1)
+                    : change.rightPointer.slice(rightParentPointer.length + 1),
+        };
+
+        if (change instanceof PropertyLeftOnly) {
+            changeOutput.leftValue = change.element;
+        } else if (change instanceof PropertyRightOnly) {
+            changeOutput.rightValue = change.element;
+        } else if (change instanceof PropertyChanged) {
+            changeOutput.leftValue = change.leftElement;
+            changeOutput.rightValue = change.rightElement;
+        }
+        detailsList.push(changeOutput);
+    }
+}
 
 export function PerformControlLevelComparison(
     comparison: ArrayChanged,
@@ -78,6 +123,12 @@ export function PerformControlLevelComparison(
             const leftControl = leftDocument.resolve(subElems.leftPointer);
             const rightControl = rightDocument.resolve(subElems.rightPointer);
 
+            const changes: ChangeDetails[] = [];
+
+            subElems.changes.forEach((change) =>
+                FlattenControlChanges(change, changes, leftControl.pointer, rightControl.pointer),
+            );
+
             return {
                 leftIdentifiers: {
                     id: leftControl.resolve('id').raw,
@@ -88,32 +139,12 @@ export function PerformControlLevelComparison(
                     title: rightControl.resolve('title').raw,
                 },
                 status: subElems.changes.length > 0 ? 'changed' : 'ok',
-                changes: subElems.changes.map((change) => {
-                    if (change instanceof PropertyLeftOnly) {
-                        return {
-                            field: change.leftPointer,
-                            leftValue: change.element,
-                        };
-                    } else if (change instanceof PropertyRightOnly) {
-                        return {
-                            field: change.rightPointer,
-                            rightValue: change.element,
-                        };
-                    } else if (change instanceof PropertyChanged) {
-                        return {
-                            field: change.rightPointer,
-                            leftValue: change.leftElement,
-                            rightValue: change.rightElement,
-                        };
-                    } else {
-                        return {
-                            field: change.leftPointer,
-                        };
-                    }
-                }),
+                changes,
             } as ControlLevelComparison;
         }),
     );
+
+    sortControlLevelComparison(controlLevelComparisons);
 
     return controlLevelComparisons;
 }
